b064a66bcb8bd85e1cdd7ca297c05e5f1a012a1b
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-06-28 21:46:07 +0600

    e.cpp

---
+++ university/computer_club/e.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double r,a,c;
+    cin>>r;
+    a = M_PI*r*r;
+    c = 2*M_PI*r;
+
+    cout<<fixed<<setprecision(6)<<a<<" "<<fixed<<setprecision(6)<<c<<endl;
+    
+    return 0;
+}
