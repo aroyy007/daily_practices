e1626f186b50198b65f3768538d3659c4f3bf2b4
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-06-28 21:51:39 +0600

    p.cpp

---
+++ university/computer_club/p.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,y;
+    while(cin>>x>>y)
+    {
+        if(x==0 && y==0)
+        {
+            break;
+        }   
+        cout<<min(x,y)<<" "<<max(x,y)<<endl;
+    }
+    return 0;
+}
