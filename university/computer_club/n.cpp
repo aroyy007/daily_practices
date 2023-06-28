dd12bf1106af1a831dfc210c806ad995cf97b64d
Arijit Roy <120275227+aroyy007@users.noreply.github.com>
2023-06-28 21:50:23 +0600

    n.cpp

---
+++ university/computer_club/n.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int a,i=1;
+    while (cin >> a)
+    {
+        if (a == 0)
+        {
+            break;
+        }
+        cout << "Case " << i  << ": " << a << endl;
+        i++;
+    }
+
+    return 0;
+}
